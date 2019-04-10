13fdecda0e48bda8f7645d6ba121981d206389fd
董金叶 <spiroo@126.com>
2019-04-10 16:50:09 +0800

    clear

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-px2rem': {
-      remUnit: 37.5
-    }
-  }
-};
--- src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  requestLoading: state => state.app.requestLoading,
-  token: state => state.user.token,
-  userList: state => state.user.userList,
-  name: state => state.user.name
-};
-export default getters;
